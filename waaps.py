58d6ba2dc121e835226b2e0744352ca4ecf6687c
Ajeet Jha <ajeetjha1@gmail.com>
2014-06-29 00:54:25 +0530

    Amazon review Scraping
    Additional scraping for aamazon review link
    
    Signed-off-by: Ajeet Jha <ajeetjha1@gmail.com>

--- waaps.py
+++ waaps.py
@@ -26,10 +26,30 @@ class reviewsCollector:
                     print "\n\n"
                 start+=10
 
+    def traverseAmazonReviews (self):
+        url = self.baseUrl
+        url += "?pageNumber="
+        start = 1
+        while True:
+            reviewPage = requests.get (url + str(start))
+            print "Printing url(Amazon) " + url + str(start)
+            raw_input("continue ....")
+            reviewPageHtml = html.fromstring (reviewPage.text)
+            reviewList = reviewPageHtml.xpath('//div[@class="reviewText"]/text()')
+            if len(reviewList) == 0:
+                break
+            else:
+                for review in reviewList:
+                    print review
+                    print "\n\n"
+                start += 1
+
     def traverseAllReviews (self, urlType="flipkart"):
         ''' Depending on urlType traverse amazon or flipkart review pages '''
         if urlType == "flipkart":
             self.traverseFlipkartReviews()
+        elif urlType == "amazon" :
+            self.traverseAmazonReviews()
         else :
             pass
 
@@ -37,8 +57,9 @@ def main ():
     r = reviewsCollector()
     url1 = "http://www.flipkart.com/seagate-backup-plus-1-tb-external-hard-disk/p/itmdmyf9kzkvsx8a"
     url = "http://www.flipkart.com/seagate-backup-plus-1-tb-external-hard-disk/product-reviews/ITMDMYF9KZKVSX8A"
-    r.setBaseProductUrl (url)
-    r.traverseAllReviews()
+    url2 = "http://www.amazon.in/product-reviews/B009PJG3MQ"
+    r.setBaseProductUrl (url2)
+    r.traverseAllReviews("amazon")
 
 if __name__ == "__main__":
     main()
